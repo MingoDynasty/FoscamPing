c8474019d720ac3800d024ee363cd0ad36a71873
Mingo <a@b.c>
2015-10-20 20:23:22 -0400

    added missing semi colon

--- database_queries.sql
+++ database_queries.sql
@@ -1,7 +1,7 @@
 -- Get a latest ping result for each device.
 Select p.device_id, p.date_pinged, packets_sent, packets_received, packets_lost, minimum_ping, maximum_ping, average_ping, is_success 
 From ping_results p Inner Join (Select device_id, max(DATE_PINGED) as DATE_PINGED From ping_results Group By device_id) q 
-On p.device_id = q.device_id And p.DATE_PINGED = q.DATE_PINGED
+On p.device_id = q.device_id And p.DATE_PINGED = q.DATE_PINGED;
 
 -- Get a latest ping result for each active device.
 SELECT r.device_id, hostname, r.date_pinged, packets_sent, packets_received, packets_lost, minimum_ping, maximum_ping, average_ping, is_success
