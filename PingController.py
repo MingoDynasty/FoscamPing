6502998df430f0a00b19c7af6016fd965ac53028
Mingo <a@b.c>
2015-10-25 07:49:53 -0400

    Changed subprocess call shell=True to shell=False for security. Changed command to be passed as a list for security. When a ping is failed, the subprocess output is now logged.

--- PingController.py
+++ PingController.py
@@ -1,5 +1,6 @@
 import logging  # Provides access to logging api.
 import subprocess  # Provides ability to spawn new processes.
+import shlex #  Simple Lexical Analysis
 from NamedTuples import PingResult
 
 
@@ -19,18 +20,20 @@ class PingController:
         :return: PingResult on successful ping, else None.
         """
         self.logger.debug("Pinging hostname: " + hostname)
-        cmd = "ping -n 1 " + hostname
+        cmd = "ping -n 1 " + hostname  # original command
+        cmd_list = shlex.split(cmd)  # split the command in a list
 
         try:
-            subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+            subprocess.check_output(cmd_list, stderr=subprocess.STDOUT, shell=False)
             pingResult = PingResult(device_id=deviceId, is_success=True)
             self.logger.info("Successfully pinged: " + hostname)
             return pingResult  # if we make it here, then the command succeeded
 
         # exception is thrown on non-zero exits, so this must mean the command failed
         except subprocess.CalledProcessError as e:
-            # TODO: may want to return e object or something
+            # TODO: may want to return e object or perhaps just raise and let caller handle the exception
             self.logger.warning("Failed to ping: " + hostname)
+            self.logger.debug(e.output)
             return None
 
 # end PingController
