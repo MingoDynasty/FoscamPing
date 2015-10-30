9a38d9f598e2a677178d8535693bae02228da4e5
Mingo <a@b.c>
2015-10-30 00:37:03 -0400

    Changed ping count to use default count of 4 instead of 1. This should reduce the chance of marking a device as down when it is still intermittently available. Added ping output parsing. Much more ping data is now collected and saved to database. Fixed a bug where if all ping requests timed out, or if the destination host was unreachable, then the device would still be marked as up.

--- PingController.py
+++ PingController.py
@@ -1,7 +1,9 @@
 import logging  # Provides access to logging api.
 import subprocess  # Provides ability to spawn new processes.
-import shlex #  Simple Lexical Analysis
+import shlex  # Simple Lexical Analysis
+import re  # Regular Expressions
 from NamedTuples import PingResult
+from PingResultPojo import PingResultPojo
 
 
 class PingController:
@@ -20,12 +22,28 @@ class PingController:
         :return: PingResult on successful ping, else None.
         """
         self.logger.debug("Pinging hostname: " + hostname)
-        cmd = "ping -n 1 " + hostname  # original command
+        # cmd = "ping -n 1 " + hostname  # original command
+        cmd = "ping " + hostname  # original command
         cmd_list = shlex.split(cmd)  # split the command in a list
 
         try:
-            subprocess.check_output(cmd_list, stderr=subprocess.STDOUT, shell=False)
-            pingResult = PingResult(device_id=deviceId, is_success=True)
+            output = subprocess.check_output(cmd_list, stderr=subprocess.STDOUT, shell=False, universal_newlines=False)
+
+            # entirely possible to get a successful return code but the ping actually failed
+            # parse the output and collect additional data from it
+            output_decoded = output.decode("utf-8")
+            pingResultPojo = self.parse(output_decoded)
+
+            # extract pojo and put into namedtuple
+            pingResult = PingResult(device_id=deviceId,
+                                    date_pinged=None,
+                                    packets_sent=float(pingResultPojo.packets_sent),
+                                    packets_received=float(pingResultPojo.packets_received),
+                                    packets_lost=float(pingResultPojo.packets_lost),
+                                    minimum_ping=float(pingResultPojo.minimum_ping),
+                                    maximum_ping=float(pingResultPojo.maximum_ping),
+                                    average_ping=float(pingResultPojo.average_ping),
+                                    is_success=True)
             self.logger.info("Successfully pinged: " + hostname)
             return pingResult  # if we make it here, then the command succeeded
 
@@ -33,7 +51,41 @@ class PingController:
         except subprocess.CalledProcessError as e:
             # TODO: may want to return e object or perhaps just raise and let caller handle the exception
             self.logger.warning("Failed to ping: " + hostname)
-            self.logger.debug(e.output)
+            self.logger.debug("Output: %s" % e.output.strip())
             return None
+
+    def parse(self, ping_output):
+        """
+        Parse the ping output.
+        :param ping_output: string to parse.
+        :return: PingResultPojo with ping result data, else None on failure.
+        """
+        if 'Destination host unreachable' in ping_output:
+            self.logger.debug("Destination host was unreachable.")
+            return None
+
+        # can usually get these
+        sent = ping_output.split('Sent = ')[1].split(',')[0]
+        received = ping_output.split('Received = ')[1].split(',')[0]
+        lost = ping_output.split('Lost = ')[1].split(',')[0]
+        lostTotal = ping_output.split('Lost = ')[1].split(' ')[0]
+        lostPercent = ping_output.split('%')[0].split('(')[1]
+
+        # might not get these if there's "Destination host unreachable"
+        min_ping = ping_output.split('Minimum = ')[1].split('ms')[0]
+        max_ping = ping_output.split('Maximum = ')[1].split('ms')[0]
+        avg_ping = ping_output.split('Average = ')[1].split('ms')[0]
+
+        # package results into pojo
+        pingResultPojo = PingResultPojo()
+        pingResultPojo.packets_sent = sent
+        pingResultPojo.packets_received = received
+        pingResultPojo.packets_lost = lostTotal
+        pingResultPojo.minimum_ping = min_ping
+        pingResultPojo.maximum_ping = max_ping
+        pingResultPojo.average_ping = avg_ping
+        pingResultPojo.is_success = True
+        self.logger.debug(vars(pingResultPojo))
+        return pingResultPojo
 
 # end PingController
---
+++ PingResultPojo.py
@@ -0,0 +1,12 @@
+class PingResultPojo:
+    device_id = None
+    date_pinged = None
+    packets_sent = None
+    packets_received = None
+    packets_lost = None
+    minimum_ping = None
+    maximum_ping = None
+    average_ping = None
+    is_success = False
+
+# end PingResultPojo
